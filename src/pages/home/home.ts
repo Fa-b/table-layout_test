63ca8bcc21aad5a54bf18b4eae12759405625c0d
Fabian Ludwig <fabian.ludwig@iontis.de>
2019-02-15 12:59:24 +0100

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as Table from 'table-layout'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  private data = [{
+    col1: 'row1 col1',
+    col2: 'row1 col2 '
+  },{
+    col1: 'row2 col1',
+    col2: 'row2 col2 '
+  },{
+    col1: 'row3 col1',
+    col2: 'row3 col2 '
+  }];
+  private table;
   constructor(public navCtrl: NavController) {
-
+    this.table = new Table(this.data);
   }
 
 }
